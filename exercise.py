68aa9110516381bf4e0701b69cc0a7207bbcdef5
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-13 13:46:08 -0500

    init commit

---
+++ exercise.py
@@ -0,0 +1,4 @@
+# Define a function that accepts a percentage as an argument and returns a letter grade (A+, A, A-, B+, etc) for that
+# percentage.
+#
+# Prompt your user to enter a percentage and display a message showing them the equivalent letter grade.
